98f229b4a57e6fbc93aeadb747d80a9eefbeabde
Padmanabha Das <padmanabhadas9647@gmail.com>
2022-08-11 00:24:34 +0530

    insertNodeAtPosition, deleteFirstNode, deleteLastNode, deleteNodeAtPosition

--- LinkedList/src/SinglyLinkedList.java
+++ LinkedList/src/SinglyLinkedList.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class SinglyLinkedList {
@@ -15,6 +16,7 @@ public class SinglyLinkedList {
         }
     }
 
+    // choice 1
     public static void createSinglyLinkedList(int value) {
         ListNode newnode = new ListNode ( value );
         if (head == null) {
@@ -27,6 +29,28 @@ public class SinglyLinkedList {
         tail = newnode;
     }
 
+    // choice 2
+    public static void displayLinkedList() {
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            System.out.print ( currentNode.data + " --> " );
+            currentNode = currentNode.next;
+        }
+        System.out.print ( "null\n" );
+    }
+
+    // choice 3
+    public static int length() {
+        int noOfNodes = 0;
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            noOfNodes++;
+            currentNode = currentNode.next;
+        }
+        return noOfNodes;
+    }
+
+    // choice 4
     public static void insertNodeAtBeginning(int value) {
         ListNode newnode = new ListNode ( value );
         if (head != null) {
@@ -35,6 +59,7 @@ public class SinglyLinkedList {
         head = newnode;
     }
 
+    // choice 5
     public static void insertNodeAtEnd(int value) {
         ListNode newnode = new ListNode ( value );
         // using 2 pointer, head & tail
@@ -54,29 +79,87 @@ public class SinglyLinkedList {
         currentNode.next = newnode;
     }
 
-    public static void displayLinkedList() {
-        ListNode currentNode = head;
-        while (currentNode != null) {
-            System.out.print ( currentNode.data + " --> " );
-            currentNode = currentNode.next;
+    // choice 6
+    public static void insertNodeAtPosition(int value, int pos) {
+        ListNode newnode = new ListNode ( value );
+        if (pos > length ( ) + 1) {
+            System.out.println ( "Length must be greater than " + pos );
+            return;
         }
-        System.out.print ( "null\n" );
+        if (head == null) {
+            head = newnode;
+        } else if (pos == 1) {
+            newnode.next = head;
+            head = newnode;
+        } else {
+            ListNode current = head;
+            for (int i = 1; i < pos - 1 && current.next != null; i++) {
+                current = current.next;
+            }
+            newnode.next = current.next;
+            current.next = newnode;
+        }
     }
 
-    public static int length() {
-        int noOfNodes = 0;
+    // choice 7
+    public static ListNode deleteFirstNode() {
+        if (head == null) {
+            System.out.println ( "Linked List is already empty" );
+            return null;
+        }
+        ListNode firstNode = head;
+        head = head.next;
+        firstNode.next = null;
+        return firstNode;
+    }
+
+    // choice 8
+    public static int deleteLastNode() {
+        if (head == null) {
+            System.out.println ( "Linked List already empty" );
+            return -1;
+        } else if (head.next == null) {
+            int dataToBeDeleted = head.data;
+            head = null;
+            return dataToBeDeleted;
+        }
+        ListNode prevLastNode = null;
         ListNode currentNode = head;
-        while (currentNode != null) {
-            noOfNodes++;
+        while (currentNode.next != null) {
+            prevLastNode = currentNode;
             currentNode = currentNode.next;
         }
-        return noOfNodes;
+        prevLastNode.next = null;
+        return currentNode.data;
+    }
+
+    // choice 9
+    public static int deleteNodeAtPosition(int pos) {
+        if (pos >= length ( ) + 1) {
+            System.out.println ( "Length must be greater than " + pos );
+            return 0;
+        }
+        if (head == null) {
+            System.out.println ( "Linked List already empty" );
+            return -1;
+        } else if (pos == 1) {
+            head = head.next;
+            return pos;
+        } else {
+            ListNode currentNode;
+            ListNode previousNode = head;
+            for (int i = 1; i < pos - 1; i++)
+                previousNode = previousNode.next;
+            currentNode = previousNode.next;
+            previousNode.next = currentNode.next;
+            return pos;
+        }
     }
 
     public static void main(String[] args) {
 //        SinglyLinkedList singlyLinkedList = new SinglyLinkedList ( );
         Scanner scanner = new Scanner ( System.in );
-        int length, nodeData = 0;
+        int length, nodeData, pos;
         int choice;
         do {
             System.out.print ( "Enter your choice: " );
@@ -101,6 +184,30 @@ public class SinglyLinkedList {
                 System.out.print ( "Enter data to be added at end: " );
                 nodeData = scanner.nextInt ( );
                 insertNodeAtEnd ( nodeData );
+            } else if (choice == 6) {
+                System.out.print ( "Enter position: " );
+                pos = scanner.nextInt ( );
+                if (pos == 0) {
+                    System.out.println ( "Pos value must be greater than 0" );
+                    System.out.print ( "Enter your choice: " );
+                    choice = scanner.nextInt ( );
+                }
+                System.out.print ( "Enter data to be added at position " + pos + ": " );
+                nodeData = scanner.nextInt ( );
+                insertNodeAtPosition ( nodeData, pos );
+            } else if (choice == 7) {
+                System.out.println ( Objects.requireNonNull ( deleteFirstNode ( ) ).data + " deleted" );
+            } else if (choice == 8) {
+                System.out.println ( deleteLastNode ( ) + " deleted successfully" );
+            } else if (choice == 9) {
+                System.out.print ( "Enter position: " );
+                pos = scanner.nextInt ( );
+                if (pos == 0) {
+                    System.out.println ( "Pos value must be greater than 0" );
+                    System.out.print ( "Enter your choice: " );
+                    choice = scanner.nextInt ( );
+                }
+                System.out.println ( "Data from " + deleteNodeAtPosition ( pos ) + " deleted successfully" );
             }
         } while (choice != 100);
     }
